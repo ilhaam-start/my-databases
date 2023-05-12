e551470a7483f3b4ab8f7127a113c447480d4642
Ilhaam <ilhaam777@gmail.com>
2023-05-12 12:38:34 +0100

    "update"

--- my-databases/music_library/lib/album_repository.py
+++ my-databases/music_library/lib/album_repository.py
@@ -26,7 +26,7 @@ class AlbumRepository:
     # Do you want to get its id back? Look into RETURNING id;
     def create(self, album):
         self._connection.execute('INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)', [
-                                 album.title, album.release_year, album.artist_id])
+                                album.title, album.release_year, album.artist_id])
         return None
     
     # Delete an album by their id
