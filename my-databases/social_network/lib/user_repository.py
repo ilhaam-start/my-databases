1a74c2473a5ddd146c634849a257e991c57c5a67
Ilhaam <ilhaam777@gmail.com>
2023-05-15 10:04:18 +0100

    "completed"

--- my-databases/social_network/lib/user_repository.py
+++ my-databases/social_network/lib/user_repository.py
@@ -12,3 +12,23 @@ class UserRepository:
             item = User(user["id"], user["email_address"], user["username"])
             all_users.append(item)
         return all_users
+    
+    def find(self, id):
+        users = self._connection.execute(
+            'SELECT * from user_accounts WHERE id = %s', [id])
+        user = users[0]
+        return User(user["id"], user["email_address"], user["username"])
+    
+    def create(self, account):
+        self._connection.execute('INSERT INTO user_accounts (id, email_address, username) VALUES (%s, %s, %s)', [
+                                account.id, account.email_address, account.username])
+        return None
+    
+    def update(self, user_id, new_email, new_username):
+        self._connection.execute('UPDATE user_Accounts SET email_address = %s, username = %s WHERE id = %s', [new_email, new_username, user_id])
+        return None
+
+    def delete(self, id):
+        self._connection.execute(
+            'DELETE FROM user_accounts WHERE id = %s', [id])
+        return None
--- my-databases/social_network/tests/test_user_repository.py
+++ my-databases/social_network/tests/test_user_repository.py
@@ -8,10 +8,10 @@ def test_all_return_list(db_connection):
     db_connection.seed("seeds/social_network.sql")
     user_repo = UserRepository(db_connection)
     result = user_repo.all()
-    assert result == [
+    assert [str(result) for result in result] == [
         "User Details(1 - ilhaam777@gmail.com - ilhaam777)",
-        "User Details(2 - ilhaam.ahmed@hotmail.com - ilhaam1)"
-        "User Details(3 - ahmed@gmail.com - illy)"
+        "User Details(2 - ilhaam.ahmed@hotmail.com - ilhaam1)",
+        "User Details(3 - ahmed@gmail.com - illy)",
         "User Details(4 - il.ah@gmail.com - ilham)"
     ]
 
@@ -21,6 +21,8 @@ Test #find method
 def test_find_return_item(db_connection):
     db_connection.seed("seeds/social_network.sql")
     user_repo = UserRepository(db_connection)
+    result = user_repo.find(3)
+    assert str(result) == "User Details(3 - ahmed@gmail.com - illy)"
 
 """
 Test #create method
@@ -28,6 +30,15 @@ Test #create method
 def test_create_add_to_list(db_connection):
     db_connection.seed("seeds/social_network.sql")
     user_repo = UserRepository(db_connection)
+    user_repo.create(User(5, "hafsa.ahmed", "haffy"))
+    result = user_repo.all()
+    assert [str(result) for result in result] == [
+        "User Details(1 - ilhaam777@gmail.com - ilhaam777)",
+        "User Details(2 - ilhaam.ahmed@hotmail.com - ilhaam1)",
+        "User Details(3 - ahmed@gmail.com - illy)",
+        "User Details(4 - il.ah@gmail.com - ilham)",
+        "User Details(5 - hafsa.ahmed - haffy)"
+    ]
 
 """
 Test #update method
@@ -35,6 +46,9 @@ Test #update method
 def test_update_return_updated_list(db_connection):
     db_connection.seed("seeds/social_network.sql")
     user_repo = UserRepository(db_connection)
+    user_repo.update(2, "lalalla@gmail.com", "lalla")
+    result = user_repo.find(2)
+    assert str(result) == "User Details(2 - lalalla@gmail.com - lalla)"
 
 """
 Test #delete method
@@ -42,3 +56,10 @@ Test #delete method
 def test_delete_return_shortened_list(db_connection):
     db_connection.seed("seeds/social_network.sql")
     user_repo = UserRepository(db_connection)
+    user_repo.delete(4)
+    result = user_repo.all()
+    assert [str(result) for result in result] == [
+        "User Details(1 - ilhaam777@gmail.com - ilhaam777)",
+        "User Details(2 - ilhaam.ahmed@hotmail.com - ilhaam1)",
+        "User Details(3 - ahmed@gmail.com - illy)"
+    ]
