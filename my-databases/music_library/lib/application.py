31f91d6cf69370e303c9714bb0d6831bb04240f2
Ilhaam <ilhaam777@gmail.com>
2023-05-15 10:47:13 +0100

    "updating my files"

--- my-databases/music_library/lib/application.py
+++ my-databases/music_library/lib/application.py
@@ -1,4 +1,5 @@
 from lib.artist_repository import ArtistRepository
+from lib.album_repository import AlbumRepository
 from lib.database_connection import DatabaseConnection
 
 class Application():
@@ -18,9 +19,30 @@ class Application():
 
         artist_repository = ArtistRepository(self._connection)
         artists = artist_repository.all()
+        print("Here is the list of albums:")
 
         for artist in artists:
             print(f"{artist.id}: {artist.name} ({artist.genre})")
+        
+        album_repository = AlbumRepository(self._connection)
+        albums = album_repository.all()
+        print("Here is the list of albums:")
+
+        for album in albums:
+            print(f"{album.id}: {album.title}, {album.release_year}, {album.artist_id}")
+
+        print("Welcome to the music library manager!")
+        print()
+        print("What would you like to do?")
+        print(" 1 - List all albums")
+        print(" 2 - List all artists")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print(albums)
+        elif choice == "2":
+            print(artists)
+        else:
+            print("Invalid choice!")
 
 if __name__ == '__main__':
     app = Application()
