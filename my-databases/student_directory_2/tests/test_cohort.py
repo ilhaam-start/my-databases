3bfefaa898382c433266f8db165a13f3391ddc0a
Ilhaam <ilhaam777@gmail.com>
2023-05-15 14:16:40 +0100

    "commiting latest changes"

---
+++ my-databases/student_directory_2/tests/test_cohort.py
@@ -0,0 +1,5 @@
+from lib.cohort import *
+
+"""
+cohort construct with id, name and starting_date
+"""
---
+++ my-databases/student_directory_2/tests/test_student.py
@@ -0,0 +1,10 @@
+from lib.student import *
+
+"""
+student construct with id, name and cohort_id
+"""
+# def test_student_construct():
+#     student = Student(1, "Student1", 1)
+#     assert student.id == 1
+#     assert student.name == "Student1"
+#     assert student.cohort_id == 1
