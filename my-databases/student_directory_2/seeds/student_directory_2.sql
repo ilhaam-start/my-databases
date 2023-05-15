e4519e2adcd9f010dc4103efa2290127455ed887
Ilhaam <ilhaam777@gmail.com>
2023-05-15 12:41:14 +0100

    "JOINs"

--- my-databases/student_directory_2/seeds/student_directory_2.sql
+++ my-databases/student_directory_2/seeds/student_directory_2.sql
@@ -1,13 +1,38 @@
+
+DROP TABLE IF EXISTS students;
+DROP SEQUENCE IF EXISTS students_id_seq;
+
+DROP TABLE IF EXISTS cohorts;
+DROP SEQUENCE IF EXISTS cohorts_id_seq;
+
+
+CREATE SEQUENCE IF NOT EXISTS cohorts_id_seq;
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY,
   name text,
   starting_date int
 );
 
--- Then the table with the foreign key second.
+CREATE SEQUENCE IF NOT EXISTS students_id_seq;
 CREATE TABLE students (
   id SERIAL PRIMARY KEY,
   name text,
   cohort_id int,
-  constraint fk_cohort_id foreign key(cohort_id) references cohorts(id) on delete cascade
+  constraint fk_cohort_id foreign key(cohort_id)
+      references cohorts(id)
+      on delete cascade
 );
+
+INSERT INTO cohorts (name, starting_date) VALUES ('Data Engineering', 04/2023)
+INSERT INTO cohorts (name, starting_date) VALUES ('Software Engineering', 05/2023)
+INSERT INTO cohorts (name, starting_date) VALUES ('Quality Engineering', 04/2023)
+INSERT INTO cohorts (name, starting_date) VALUES ('DevOps', 02/2023)
+
+INSERT INTO students (name, cohort_id) VALUES ('Student1', 1)
+INSERT INTO students (name, cohort_id) VALUES ('Student2', 1)
+INSERT INTO students (name, cohort_id) VALUES ('Student3', 2)
+INSERT INTO students (name, cohort_id) VALUES ('Student4', 3)
+INSERT INTO students (name, cohort_id) VALUES ('Student5', 3)
+INSERT INTO students (name, cohort_id) VALUES ('Student6', 4)
+INSERT INTO students (name, cohort_id) VALUES ('Student7', 2)
+INSERT INTO students (name, cohort_id) VALUES ('Student8', 1)
