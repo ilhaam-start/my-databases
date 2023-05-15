fd7d1d1ea5423ebf01819be9a5f195fd5e4932c5
Ilhaam <ilhaam777@gmail.com>
2023-05-15 14:30:53 +0100

    "latest commit, gonna try an solve the pip issues"

--- my-databases/student_directory_2/tests/test_cohort.py
+++ my-databases/student_directory_2/tests/test_cohort.py
@@ -3,3 +3,15 @@ from lib.cohort import *
 """
 cohort construct with id, name and starting_date
 """
+def test_cohort_construct():
+    cohort = Cohort(1, "Data Engineering", 04/2023)
+    assert cohort.id == 1
+    assert cohort.name == "Data Engineering"
+    assert cohort.starting_date == 04/2023
+
+# """
+# Test the format as string
+# """
+# def test_format_as_string():
+#     cohort = Cohort(1, "Data Engineering", 04/2023)
+#     assert str(cohort) == "1 - Data Engineering - 04/2023"
--- my-databases/student_directory_2/tests/test_student.py
+++ my-databases/student_directory_2/tests/test_student.py
@@ -3,8 +3,15 @@ from lib.student import *
 """
 student construct with id, name and cohort_id
 """
-# def test_student_construct():
-#     student = Student(1, "Student1", 1)
-#     assert student.id == 1
-#     assert student.name == "Student1"
-#     assert student.cohort_id == 1
+def test_student_construct():
+    student = Student(1, "Student1", 1)
+    assert student.id == 1
+    assert student.name == "Student1"
+    assert student.cohort_id == 1
+
+"""
+Test the format as string
+"""
+def test_format_as_string():
+    student = Student(1, "Student1", 1)
+    assert str(student) == "1 - Student1 - 1"
